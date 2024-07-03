c30e45c3f55da2dc2111febcf9ad1e2786d6be8a
Gerdes88 <73107506+Gerdes88@users.noreply.github.com>
2024-07-03 10:49:32 +0200

    Første version der virker

--- TemperatureMonitoring/TemperatureMonitoring.ino
+++ TemperatureMonitoring/TemperatureMonitoring.ino
@@ -2,112 +2,101 @@
 #include <DallasTemperature.h>
 #include <SoftwareSerial.h>
 
-#define ONE_WIRE_BUS 13
-#define ESP8266_RX 2
-#define ESP8266_TX 3
+#define ONE_WIRE_BUS 13  // DS18B20 data wire er tilsluttet til digital pin 2
+#define ESP8266_RX 3
+#define ESP8266_TX 2
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-SoftwareSerial esp8266(ESP8266_RX, ESP8266_TX); // RX, TX
+SoftwareSerial esp8266(ESP8266_RX, ESP8266_TX);
+
+const char* SSID = "MeyerGerdesNet";
+const char* PASSWORD = "3jbyd4lsv3j172+2600";
+//const char* SSID = "Martins24";
+//const char* PASSWORD = "12345678";
+const char* API_KEY = "H3C2WB9XWVSR0BE8";
 
 void setup() {
-  esp8266.begin(9600);  // Start seriel kommunikation med computeren
-  esp8266.begin(9600);    // Start seriel kommunikation med ESP8266 (ændret til 9600)
-  sensors.begin();        // Start Dallas Temperature sensor
-
-  Serial.println("ESP8266 AT Kommando Test og Temperatur Måling");
-
-  // Initialiser ESP8266
-  initESP8266();
+  Serial.begin(115200);
+  esp8266.begin(115200);
+  sensors.begin();
+  connectWiFi();
 }
 
 void loop() {
-  // Temperatur måling
+   float temp = getTemperature();
+  if (sendToThingSpeak(temp)) {
+    Serial.println("Data sent successfully");
+  } else {
+    Serial.println("Failed to send data");
+  }
+  delay(20000); 
+}
+
+float getTemperature() {
   sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
-
-  if (temperatureC == DEVICE_DISCONNECTED_C) {
-    Serial.println("Fejl: Kunne ikke læse temperaturdata");
-    delay(1000);
-    return;
-  }
-
-  float temperatureF = sensors.toFahrenheit(temperatureC);
-
-  Serial.print("Temperatur: ");
-  Serial.print(temperatureC);
-  Serial.print(" C eller ");
-  Serial.print(temperatureF);
-  Serial.println(" F");
-
-  // Send temperatur data til ESP8266
-  sendTemperatureData(temperatureC);
-
-  // Håndter ESP8266 kommunikation
-  handleESP8266Communication();
-
-  delay(2000);  // Vent 2 sekunder før næste måling
+  float tempC = sensors.getTempCByIndex(0);
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.println(" C");
+  return tempC;
 }
 
-void initESP8266() {
-  if (!sendCommandWithCheck("AT", "OK")) {
-    Serial.println("ESP8266 ikke fundet. Kontroller forbindelser og baud rate.");
-    while(1);  // Stop programmet hvis ESP8266 ikke svarer
-  }
-  //delay(5000);
-  sendCommand("AT");
-  sendCommand("AT+RST");
-  delay(1000);
-  sendCommand("AT+CWMODE=1");  // Sæt ESP8266 i station mode
-  sendCommand("AT+CWJAP=\"Martins24\",\"12345678\"");  // Forbind til WiFi
-  delay(5000);  // Vent på WiFi forbindelse
-  sendCommand("AT+CIFSR");  // Få IP-adresse
-  sendCommand("AT+CIPMUX=1");  // Konfigurer for multiple forbindelser
-  sendCommand("AT+CIPSERVER=1,80");  // Start server på port 80
-  if (!sendCommandWithCheck("AT+CWJAP=\"Martins24\",\"12345678\"", "OK")) {
-    Serial.println("Kunne ikke forbinde til WiFi. Kontroller SSID og password.");
-    while(1);
-  }
-  Serial.println("Forbundet til WiFi");
-}
-
-bool sendCommandWithCheck(String command, String expectedResponse) {
-  sendCommand(command);
-  return esp8266.find(expectedResponse.c_str());
-}
-
-void sendTemperatureData(float temperature) {
-  String data = String(temperature, 2);  // Konverter float til string med 2 decimaler
-  String command = "AT+CIPSEND=0," + String(data.length()) + "\r\n";
-  sendCommand(command);
-  delay(100);
-  esp8266.print(data);
-}
-
-void handleESP8266Communication() {
-  while (esp8266.available()) {
-    Serial.write(esp8266.read());
-  }
+void connectWiFi() {
+  int wifiConnectRetries=20;
+  Serial.println("Connecting to WiFi...");
+  //Serial.println("List of available wifi: "+esp8266.println("AT+CWLAP"));
+  esp8266.println("AT+RST");
+  delay(2000);
+  esp8266.println("AT+CWMODE=1");
+  delay(5000);
+  String cmd = "AT+CWJAP=\"";
+  cmd += SSID;
+  cmd += "\",\"";
+  cmd += PASSWORD;
+  cmd += "\"";
   
-  while (Serial.available()) {
-    esp8266.write(Serial.read());
-  }
+  while (wifiConnectRetries>0)
+  {
+    esp8266.println(cmd);
+    delay(2000);
+    if (esp8266.find("OK")) {
+      Serial.println("Connected to WiFi with this cmd: "+cmd);
+      wifiConnectRetries = 0;
+    } else {
+      Serial.println("Failed to connect to WiFi, attempting again with: "+ String(wifiConnectRetries) + " wifiConnectRetries left, using this cmd: "+cmd);
+      wifiConnectRetries = wifiConnectRetries-1;
+    }
+  } 
 }
 
-void sendCommand(String command) {
-  esp8266.flush();
+bool sendToThingSpeak(float temp) {
+  String cmd = "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80";
+  esp8266.println(cmd);
+  delay(2000);
+  
+  String getData = "GET /update?api_key=";
+  getData += API_KEY;
+  getData += "&field1=";
+  getData += String(temp);
+  getData += " HTTP/1.1\r\n";
+  getData += "Host: api.thingspeak.com\r\n\r\n";
+  
+  cmd = "AT+CIPSEND=";
+  cmd += String(getData.length());
+  esp8266.println(cmd);
+  Serial.println("Sending API data: "+ getData);
+  delay(1000);
+  
+  esp8266.print(getData);
+  delay(1000);
+  
+  esp8266.println("AT+CIPCLOSE");
+  return true;
+}
+
+void clearSerialBuffer() {
   while (esp8266.available()) {
     esp8266.read();
   }
-  esp8266.println(command);
-  delay(1000);
-  while (esp8266.available()) {
-    char c = esp8266.read();
-    if (c == '\r' || c == '\n') {
-      Serial.println();
-    } else {
-      Serial.write(c);
-    }
-  }
-  Serial.println();
 }
